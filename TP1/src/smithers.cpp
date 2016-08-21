93ac124c1051a5af3c9db8fb5888567d9088c3df
Jonás Levy Alfie <jonaslevy5@gmail.com>
2016-08-21 14:10:38 -0300

    Smithers: código más bonito (ya me aburrí)

--- TP1/src/smithers.cpp
+++ TP1/src/smithers.cpp
@@ -22,50 +22,72 @@ vi bitsEn1(int L) {
     return res;
 }
 
-int main(){
-    int N, L;
-    while(cin >> N >> L) {
-        vi matRes(9); 
-        for(auto &d : matRes) {
-            cin >> d;
-        }
-        Matriz matrizResultado(matRes);
-        vm matrices(N);
-        for(int i=0; i<N; i++) {
-            vi mat(9);
-            for(auto &d : mat) {
-                cin >> d;
-            }
-            matrices[i] = Matriz(mat);
-        }
+class subArreglosDeL {
+    const vm &M;
+    vector<vm> productos;
+    
+public:
+    subArreglosDeL(const vm &matrices) : M(matrices) {
+        int N = (int)M.size();
         int exp = maxExp2Menor(N)+1;
-        vector<vm> prods(N);
-        // Inicializamos todos los productos con el primer elemento
-        for(int i=0; i<N; i++) {
-            prods[i] = vm(exp, matrices[i]);
-        }
-        // Llenamos la tabla de productorias
+        productos = vector<vm>(N);
+        
+        // productos[i][j] posee el producto del arreglo que empieza en 
+        // i con longitud 2^j. 
+        // Inicializamos todos los productos con el primer elemento.
+        for(int i=0; i<N; i++) productos[i] = vm(exp, M[i]);
+        
+        // Llenamos la tabla de productorias. 
+        // Inicialmente, productos[i][j] = productos[i][j-1], es decir, 
+        // la potencia anterior. De ser posible, se agrega el subarreglo 
+        // siguiente de tamaño 2^(j-1).
         for(int j=1; j<exp; j++) {
             for(int i=0; i<N; i++) {
+                productos[i][j] = productos[i][j-1];
                 int offset = (1 << (j-1));
-                prods[i][j] = prods[i][j-1];
-                if(i+offset < N) {
-                    prods[i][j] *= prods[i+offset][j-1];
-                }
+                if(i+offset < N) productos[i][j] *= productos[i+offset][j-1];
             }
         }
-        // Calculamos las productorias de sub-arreglos de largo L 
-        bool hayResultado = false;
+    }
+    
+    vm calcularSubArreglos(int L) {
+        // Calculamos las productorias de sub-arreglos de largo L. 
+        // Usando producto[][] y el desarrollo binario de L, podemos 
+        // construir los sub-arreglos de tamaño L en log(L), dado que 
+        // un número L tendrá log(L) bits, y como mucho log(L) bits en 1. 
+        int N = (int)M.size();
         vi bitsL = bitsEn1(L);
+        vm subArreglos;
         for(int i=0; i<(N-L+1); i++) {
             int idx=i;
             Matriz res = id();
             for(auto b : bitsL) {
-                res *= prods[idx][b];
+                res *= productos[idx][b];
                 idx += (1<<b);
             }
-            hayResultado |= (res == matrizResultado);
+            subArreglos.push_back(res);
         }
+        return subArreglos;
+    }
+};
+
+int main(){
+    int N, L;
+    while(cin >> N >> L) {
+        vi matRes(9); 
+        for(auto &d : matRes) cin >> d;
+        Matriz matrizResultado(matRes);
+        vm matrices(N);
+        for(int i=0; i<N; i++) {
+            vi mat(9);
+            for(auto &d : mat) cin >> d;
+            matrices[i] = Matriz(mat);
+        }
+        subArreglosDeL subArreglosL(matrices);
+        vm arreglosDeL = subArreglosL.calcularSubArreglos(L);
+        
+        bool hayResultado = false;
+        for(auto &m : arreglosDeL) hayResultado |= (m == matrizResultado);
         if(hayResultado) 
             cout << "SI\n";
         else 
