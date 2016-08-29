e24d5b664ad83de39e3fa6d6a34f3dd1def48cfe
ucraman <ucraman@imachine.org>
2016-08-29 07:09:07 -0300

    La parte de frink

--- TP1/src/frink/frink.cpp
+++ TP1/src/frink/frink.cpp
@@ -3,34 +3,18 @@
 #define forn(i,n) for(int i=0;i<(int)(n); i++)
 
 using namespace std;
-/*
- * Idea:
- * Miro el grafico de las sumas acumuladas, reseteando cuando me paso
- * de 0. Y me voy quedando con los maximos.
- * 
- * Por que funciona: induccion en n
- * 	si n=1 entonces devuelve max(a_1,0)
- *  supongo que vale para n-1, veamos que vale para n
- * hasta el n-1 tengo el maxGlobal y el maxEnd entonces
- * si maxEnd+a_n es mayor que maxGlobal tengo una
- * sucesion que suma mas y termina en a_n entonces 
- * le asigno este valor a maxGlobal, 
- * sino maxGloba sigue siendo la que suma mas.
- * 
- * 
- * complejidad, es lineal en n 
-*/
-
 
 int main()
 {
 	int tmp,n;
-	int maxGlobal=0;//me voy guardando el maximo hasta ahora
-	int maxEnd=0;//me voy guardando el maximo que termina en el i en el que estoy parado
+	int maxGlobal=0;
+	int maxEnd=0;
 	cin >> n;
 	forn(i,n){
 		cin >> tmp;
-		maxEnd = max(maxEnd+tmp,0);//si me paso a negativo reseteo
+		//voy llevando la suma parcial truncada
+		maxEnd = max(maxEnd+tmp,0);
+		//actualizo el maximo global 
 		maxGlobal = max(maxEnd,maxGlobal);
 	}
 	cout << maxGlobal << endl;
