a6c77b7d9599d14b612b2986b4ad56914d4b35a6
Facundo Gutiérrez <elgutyy@gmail.com>
2016-09-07 02:12:51 -0300

    Retoques cosméticos al código de Lisa

--- TP1/src/lisa/lisa.cpp
+++ TP1/src/lisa/lisa.cpp
@@ -4,13 +4,15 @@
 
 using namespace std;
 
-typedef long long tint; // uso long long porque me gusta, no hace falta :-P
+typedef long long tint; 
 
 #define forn(i,n) for(tint i = 0; i < tint (n); i++)
 #define forsn(i,s,n) for(tint i = tint(s); i < tint (n); i++)
 
-//Complejidad : O(log(mask)), o sea O(n), pues mask <= 2^n
-tint cantidadAmigas (tint mask) // Cuenta la cantidad de unos (amigas) en una mascara 
+// Cuenta la cantidad de unos (amigas) en una mascara 
+// Complejidad : O(log(mask)), o sea O(n), pues mask <= 2^n
+
+tint cantidadAmigas (tint mask) 
 {
 	tint ans = 0;
 	while (mask > 0)
@@ -21,12 +23,19 @@ tint cantidadAmigas (tint mask) // Cuenta la cantidad de unos (amigas) en una ma
 	return ans;
 }
 
-// Complejidad : O(log(mask) + r^2), donde r es la cantidad de unos en mask, en particular nos queda O(n^2)
-tint felicidad (vector<vector<tint> > &d, tint mask) // Dada una mascara, calcula la felicidad del grupo de amigas que representa 
+// Dada una mascara, calcula la diversion del
+// grupo de amigas que representa 
+
+// Complejidad : O(log(mask) + r^2), 
+// donde r es la cantidad de unos en
+// mask, en particular nos queda O(n^2)
+
+tint diversion (vector<vector<tint> > &d, tint mask)
 {
-	tint n = d.size() - 1;
+	tint n = tint(d.size()) - 1; // indexamos de 0
 	vector<tint> amigas;
-	while (mask > 0) // Calcula los indices que tienen un uno en la mascara
+	// Calcula los indices que tienen un uno en la mascara
+	while (mask > 0) 
 	{
 		if (mask % 2)
 			amigas.push_back(n);
@@ -34,14 +43,25 @@ tint felicidad (vector<vector<tint> > &d, tint mask) // Dada una mascara, calcul
 		mask /= 2;
 	}
 	tint r = amigas.size(),f = 0;
+	// Aca usamos que la matriz es simetrica
 	forn(i,r)
 	forsn(j,i+1,r)
-		f += d[amigas[i]][amigas[j]]; // Aca usamos que la matriz es simetrica
+		f += d[amigas[i]][amigas[j]]; 
 	return f;
 } 
 
-// Complejidad (dado que se corre con mask = 2^n - 1) : O(n^2*2^n) (creo, revisar esto bien) 
-tint best(vector<tint> &dp, vector<vector<tint> > &d, tint mask) // Devuelve la respuesta al problema sobre el subconjunto que representa la mascara "mask"
+
+
+
+
+// Devuelve la respuesta al problema sobre el subconjunto
+// que representa la mascara "mask"
+
+// Complejidad (dado que se corre con mask = 2^n - 1) : O(n^2*2^n + 3^n) 
+// (ver informe para detalles) 
+
+
+tint best(vector<tint> &dp, vector<vector<tint> > &d, tint mask) 
 {
 	tint n = d.size();
 	if (dp[mask] == -1)
@@ -50,9 +70,15 @@ tint best(vector<tint> &dp, vector<vector<tint> > &d, tint mask) // Devuelve la 
 			dp[mask]  = 0;
 		else
 		{
-			dp[mask] = felicidad(d,mask); // solucion inicial: no separamos en ningun grupo
-			for(tint i = mask&(mask-1); i != 0; i = mask&(i-1)) // probamos la solucion en todas las particiones en 2 subconjuntos propios, y me quedo con la mejor.
-				if (i > (mask & (i ^ ((1 << n)-1)))) // Para evitar llamar dos veces el mismo caso, notar que el segundo numero es el "complemento de i" (en el subconjunto que representa mask)
+			// solucion inicial: no separamos en ningun grupo
+			dp[mask] = diversion(d,mask); 
+			// probamos la solucion en todas las particiones en 
+			// 2 subconjuntos propios, y nos quedamos con la mejor.
+			for(tint i = mask&(mask-1); i != 0; i = mask&(i-1)) 
+				// Para evitar llamar dos veces el mismo caso, 
+				// notar que el segundo numero es el "complemento de i" 
+				//(en el subconjunto que representa mask)
+				if (i > (mask & (i ^ ((1 << n)-1)))) 
 					dp[mask] = max(dp[mask],best(dp,d,i) + best(dp,d,mask & (i ^ ((1 << n)-1))));
 		}
 	}
@@ -63,7 +89,7 @@ tint best(vector<tint> &dp, vector<vector<tint> > &d, tint mask) // Devuelve la 
 
 int main()
 {
-	assert(freopen("entradaLisa.in", "r", stdin));
+	assert(freopen("lisa.in", "r", stdin));
 	tint n;
 	while(cin >> n)
 	{
@@ -71,7 +97,9 @@ int main()
 		forn(i,n)
 		forn(j,n)
 			cin >> d[i][j];
-		vector<tint> dp (1 << n,-1); // En dp[i] guardo la solucion al problema usando solo las amigas que tienen '1' en la representacion binaria de i 
+		// En dp[i] guardamos la solucion al problema usando solo las 
+		// amigas que tienen '1' en la representacion binaria de i 
+		vector<tint> dp (1 << n,-1);
 		cout << best(dp,d, (1 << n) - 1) << "\n"; // Devolvemos la solucion 		
 	}
 	return 0;
