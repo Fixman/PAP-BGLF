2445164086ad8bed6a36ee885c20a1facb7ffab4
Martin Fixman <martinfixman@gmail.com>
2016-10-06 09:39:15 -0300

    Agregando comentario.

--- TP2/src/ej1/ej1.cpp
+++ TP2/src/ej1/ej1.cpp
@@ -29,6 +29,7 @@ struct FlowNetwork
 		// Flujo que hay entre cada par de nodos <x, y>.
 		// Cada elemento puede ser igual a 0 (no pasa flujo), 1 (pasa flujo
 		// de x a y), o -1 (pasa flujo de y a x).
+		// flow[x][y] == 1 sii flow[y][x] == -1.
 		vector <vector <int>> flow(G.size(), vector <int> (G.size(), 0));
 
 		// Buscar caminos de augmento que mejoren el flujo máximo usando BFS.
@@ -68,11 +69,13 @@ struct FlowNetwork
 			if (!pred[T])
 				break;
 
-			// Recorrer el camino de augmento 
+			// Recorrer el camino de augmento que creamos y buscar la mínima capacidad
+			// disponible. Esta va a ser la capacidad del camino.
 			int df = 1 - flow[T][*pred[T]];
 			for (Node e = *pred[T]; pred[e]; e = *pred[e])
 				df = min(df, 1 - flow[e][*pred[e]]);
 
+			// Cambiar las capacidades de estas aristas.
 			for (Node e = T; pred[e]; e = *pred[e])
 			{
 				flow[*pred[e]][e] += df;
@@ -96,6 +99,10 @@ struct FlowNetwork
 		return to_string(n / 2 + 1) + " " + (n % 2 == 0 ? "left" : "right");
 	}
 
+	// Un nodo "left" es un nodo que tiene muchas aristas entrantes y una sola
+	// arista saliente, al correspondiente nodo "right".
+	// Por simplicidad, la fuente y el sumidero cuentan como nodos
+	// "left" y "right" a la vez.
 	Node left(int n)
 	{
 		if (n == S || n == T)
@@ -104,6 +111,10 @@ struct FlowNetwork
 		return 2 * n;
 	}
 
+	// Un nodo "right" es un nodo que tiene muchas aristas salientes y una sola
+	// arista enrante, al correspondiente nodo "left".
+	// Por simplicidad, la fuente y el sumidero cuentan como nodos
+	// "left" y "right" a la vez.
 	Node right(int n)
 	{
 		if (n == S || n == T)
@@ -112,12 +123,15 @@ struct FlowNetwork
 		return 2 * n + 1;
 	}
 
+	// Agrega un par de aristas dirigidas entre el nodo "right" correspondiente a
+	// un valor y el nodo "left" correspondiente a otro, y viceversa.
 	void addEdge(int f, int t)
 	{
 		G[right(f)].push_back(left(t));
 		G[right(t)].push_back(left(f));
 	}
 
+	// Crea una fuente, un sumidero, y un par de nodos por cada uno de los n nodos.
 	FlowNetwork(int n)
 	: S(2 * n), T(2 * n + 1)
 	{
